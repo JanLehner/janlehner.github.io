930b548622a0ebade5e65ee12a4fce5944e05943
Your Name~CLS <you@example.com>
2023-07-12 18:29:03 +0200

    Grundgerüst für die Fähigkeiten-Seite erstellt

--- Javascripts/skills/loadSkills.js
+++ Javascripts/skills/loadSkills.js
@@ -1,5 +1,21 @@
 const main = document.querySelector('main');
 
 export function loadSkills() {
-    main.innerHTML = `<h1>Coming soon.</h1>`;
+    main.innerHTML = `<div class="skillBlockType">
+    <p class="skillBockTitle">Programmiersprachen</p>
+    <div class="skillContainerIcons">
+        <a class="iconSkillsHolder" href="#"><img class="iconSkills" src="./Images/html5.png" alt="#"></a>
+        <a class="iconSkillsHolder" href="#"><img class="iconSkills" src="./Images/html5.png" alt="#"></a>
+        <a class="iconSkillsHolder" href="#"><img class="iconSkills" src="./Images/html5.png" alt="#"></a>
+        <a class="iconSkillsHolder" href="#"><img class="iconSkills" src="./Images/html5.png" alt="#"></a>
+        <a class="iconSkillsHolder" href="#"><img class="iconSkills" src="./Images/html5.png" alt="#"></a>
+        <a class="iconSkillsHolder" href="#"><img class="iconSkills" src="./Images/html5.png" alt="#"></a>
+    </div>
+</div>
+<div class="skillBlockType">
+    <p class="skillBockTitle">Modellierungssprachen</p>
+    <div class="skillContainerIcons">
+        <p>test</p>
+    </div>
+</div>`;
 }
